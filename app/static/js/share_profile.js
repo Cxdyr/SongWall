f69afa8b971f797a2c888b945b4c07dad3bcceb7
Cody Rabie <108381074+Cxdyr@users.noreply.github.com>
2025-02-21 02:06:54 -0500

    Minor error with js refactor fixed for sharing link

--- app/static/js/share_profile.js
+++ app/static/js/share_profile.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const shareButton = document.getElementById('copy-share-link');
     if (shareButton) {
         shareButton.addEventListener('click', function() {
-            const shareLink = "{{ url_for('view_profile', username=current_user.username, share='true', _external=True) | e }}";
+            const shareLink = shareButton.dataset.shareLink;
             navigator.clipboard.writeText(shareLink)
                 .then(() => {
                     alert('Link copied to clipboard!');
