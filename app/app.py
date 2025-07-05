cfe6471fb49875477c50d4fe2f16757b5c285155
Cody Rabie <108381074+Cxdyr@users.noreply.github.com>
2025-07-05 12:00:09 -0400

    Updated recommendation algorithim weights to provide more tailored recs, added social handle

--- app/app.py
+++ app/app.py
@@ -544,4 +544,4 @@ if __name__ == "__main__":
 
 
 #if __name__ == "__main__":
-#    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+ #   app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
